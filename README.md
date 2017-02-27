1d1804cc4418df588140eb66d52c12853b3fec62
Pg Biel <pgbiel@hotmail.com.br>
2017-02-27 16:31:49 -0300

    0.0.3

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # ascii-tree-cli
 CLI interface for ascii-tree
+
+## Install
+`npm install ascii-tree-cli -g`
+**Do not forget the `-g` option!**
+
+
+## Usage
+`ascii-tree`
+
+Answer the questions!
+
+## Example File
+
+```
+#/
+##Users
+###MyUser
+####Files
+###OtherUser
+####Files2
+##System
+###info.txt
+##Etc
+###nothing.txt
+```
+
+Results in:
+
+```
+/
+├─ Users
+│  ├─ MyUser
+│  │  └─ Files
+│  └─ OtherUser
+│     └─ Files2
+├─ System
+│  └─ info.txt
+└─ Etc
+   └─ nothing.txt
+```
--- index.js
+++ index.js
@@ -2,11 +2,11 @@ module.exports = function(){
   var inquirer     = require("inquirer");
   var { generate } = require("ascii-tree");
   var fs           = require("fs");
-  inquirer.prompt([{type: "list", name: "fileYesNo", message: "Will you use an editor to write down the ascii tree, or do you have file with it?", choices: ["Use editor", "Use already-existing file"]}]).then(function(answers){
+  inquirer.prompt([{type: "list", name: "fileYesNo", message: "Will you use an editor to write down the ascii tree, or do you have a file with it?", choices: ["Use editor", "Use already-existing file"]}]).then(function(answers){
     if (answers.fileYesNo == "Use editor") {
       inquirer.prompt([{type: "editor", name: "editIt", message: "Please write the tree (Example: #RootNode\\n##Sibling\\n###Sibling of Sibling)", validate: function(text){
         if (text.length < 1) return "Don't leave it empty!";
-        text = text.replace(/^\s+/, "");
+        text = text.replace(/^\s+/, "").replace(/\s+$/, "");
         try {
           generate(text);
         } catch (err) {
@@ -14,25 +14,26 @@ module.exports = function(){
         }
         return true;
       }}]).then(function(answer){
-        console.log("\n\n\x1b[1mGenerated Tree:\x1b[0m\n\n" + generate(answer.editIt.replace(/^\s+/, "")));
+        console.log("\n\n\x1b[1mGenerated Tree:\x1b[0m\n\n" + generate(answer.editIt.replace(/^\s+/, "").replace(/\s+$/, "")));
         process.exit(0);
       });
     } else {
       inquirer.prompt([{type: "input", name: "path", message: "Please insert the directory of the file. (Example: /Users/YourName/myfile.txt, or ./dir/file.txt)", validate: function(text){
         if (!(fs.existsSync(text))) return "Invalid path!";
+        if (fs.lstatSync(text).isDirectory()) return "File must not be a directory!";
         if (fs.accessSync(text, fs.R_OK)) return "No access to reading the file!";
-        var thetext = fs.readFileSync(text);
+        var thetext = fs.readFileSync(text, "utf8");
         if (thetext.length < 1) return "File must not be empty!";
         if (/^\s+$/.test(thetext)) return "File must not be empty!";
         try {
-          generate(fs.readFileSync(text).replace(/^\s+/, ""));
+          generate(thetext.replace(/^\s+/, "").replace(/\s+$/, ""));
         } catch (err) {
           return err.message;
         }
         return true;
       }}]).then(function(answer){
-        var thetext = fs.readFileSync(answer.path);
-        console.log("\n\n\x1b[1mGenerated Tree:\x1b[0m\n\n" + generate(thetext.replace(/^\s+/, "")));
+        var thetext = fs.readFileSync(answer.path, "utf8");
+        console.log("\n\n\x1b[1mGenerated Tree:\x1b[0m\n\n" + generate(thetext.replace(/^\s+/, "").replace(/\s+$/, "")));
         process.exit(0);
       });
     }
